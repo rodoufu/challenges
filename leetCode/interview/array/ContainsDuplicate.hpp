6e81403157c67d0e7ab3bfaf79a089dc2f8ec1c5
Rodolfo P A <rodoufu@gmail.com>
2019-01-29 16:58:35 -0200

    Create ContainsDuplicate.hpp

---
+++ leetCode/interview/array/ContainsDuplicate.hpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> valores;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
+            if (valores.find(*it) != valores.end()) {
+                return true;
+            }
+            valores.insert(*it);
+        }
+        return false;
+    }
+};
