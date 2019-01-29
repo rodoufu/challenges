99ece1d3805321e56849c43333356dbaad8ec0de
Rodolfo P A <rodoufu@gmail.com>
2019-01-29 17:05:52 -0200

    Create SingleNumber.hpp

---
+++ leetCode/interview/array/SingleNumber.hpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int value = nums[0];
+        for (vector<int>::iterator it = nums.begin() + 1; it != nums.end(); ++it) {
+            value ^= *it;
+        }
+        return value;
+    }
+};
