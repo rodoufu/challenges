500ec316ff0b6b235b6fe0880c6b9e6103ad7dd7
Rodolfo P A <rodoufu@gmail.com>
2019-01-29 16:49:33 -0200

    Create RotateArray.hpp

---
+++ leetCode/interview/array/RotateArray.hpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        if (nums.size() <= 1 || k == 0) {
+            return;
+        }
+        k = nums.size() - k;
+        vector<int>::iterator first = nums.begin();
+        vector<int>::iterator middle = nums.begin() + k;
+        vector<int>::iterator next = middle;
+        vector<int>::iterator last = nums.end();
+        while (first != next) {
+            swap(*first++, *next++);
+            if (next == last) {
+                next = middle;
+            } else if (first == middle) {
+                middle = next;
+            }
+        }
+    }
+};
