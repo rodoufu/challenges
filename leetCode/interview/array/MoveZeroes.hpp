d56da859caa4cc61269e221dcb14bd9f6b0b2c60
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 09:47:45 -0200

    Create MoveZeroes.hpp

---
+++ leetCode/interview/array/MoveZeroes.hpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int size = nums.size();
+        for (int i = 0; i < size - 1; i++) {
+            if (!nums[i]) {
+                for (int j = i + 1; j < size; j++) {
+                    if (nums[j]) {
+                        swap(nums[i], nums[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+};
