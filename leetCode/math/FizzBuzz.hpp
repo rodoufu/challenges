651067f739512eba1e4957b117d0375ea66778f3
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 17:03:14 -0200

    Create FizzBuzz.hpp

---
+++ leetCode/math/FizzBuzz.hpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> resp;
+        for (int i = 1; i <= n; ++i) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                resp.push_back(to_string(i));
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                resp.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                resp.push_back("Fizz");
+            } else {
+                resp.push_back("Buzz");
+            }
+        }
+        return resp;
+    }
+};
