6bc36e55a310b79877c779fe309c0c8b01dfc4b0
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 10:33:24 -0200

    Create TwoSum.hpp

---
+++ leetCode/interview/array/TwoSum.hpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> values;
+        vector<int> resp;
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            if (values.find(target - nums[i]) != values.end()) {
+                resp.push_back(values[target - nums[i]]);
+                resp.push_back(i);
+                return resp;
+            } else {
+                values.emplace(nums[i], i);
+            }
+        }
+        
+        return resp;
+    }
+};
