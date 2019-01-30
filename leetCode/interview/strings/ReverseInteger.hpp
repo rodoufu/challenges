f5e296fbe0835070c771f2377253163d2dd6a3a6
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 11:43:34 -0200

    Create ReverseInteger.hpp

---
+++ leetCode/interview/strings/ReverseInteger.hpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long resp = 0;
+        long long y = x > 0 ? x : -x;
+        while (y > 0) {
+            resp *= 10;
+            resp += y % 10;
+            y /= 10;
+        }
+        resp = resp > (1LL << 32 - 1) ? 0 : resp;
+        return (int)(x < 0 ? -resp : resp);
+    }
+};
