bd483db2c522f50742c91eed5a88555457186bfd
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 16:09:01 -0200

    Create RemoveNthNode.hpp

---
+++ leetCode/interview/LinkedList/RemoveNthNode.hpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* before = nullptr;
+        auto node = head;
+        auto it = head;
+
+        for (int i = 1; i < n; ++i) {
+            it = it->next;
+        }
+        while (it->next) {
+            before = node;
+            node = node->next;
+            it = it->next;
+        }
+        
+        if (before) {
+            before->next = node->next;
+        } else if (node == it) {
+            head = nullptr;
+        } else {
+            head = node->next;
+        }
+        delete node;
+        
+        
+        return head;
+    }
+};
