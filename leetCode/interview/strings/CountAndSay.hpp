34571255e0ed4f7945fae369bf93b283f23e6eb9
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 14:32:45 -0200

    Create CountAndSay.hpp

---
+++ leetCode/interview/strings/CountAndSay.hpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string countToSay(string &num) {
+        string resp;
+        int lennum = num.length();
+        const char * numc = num.c_str();
+        char letter = 0;
+        int count = 0;
+        for (int i = 0; i < lennum; i++) {
+            if (letter == numc[i]) {
+                ++count;
+            } else {
+                if (letter) {
+                    resp += to_string(count);
+                    resp += letter;
+                }
+                letter = numc[i];
+                count = 1;
+            }
+        }
+        if (letter) {
+            resp += to_string(count);
+            resp += letter;
+        }
+        return resp;
+    }
+    
+    string countAndSay(int n) {
+        string resp = "1";
+        for (int i = 1; i < n; i++) {
+            resp = countToSay(resp);
+        }
+        return resp;
+    }
+};
