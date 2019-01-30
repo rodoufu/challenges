6838d7bdc8234b2c7d1cdb9688dae09dc8f58e46
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 12:03:58 -0200

    Create ValidAnagram.hpp

---
+++ leetCode/interview/strings/ValidAnagram.hpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> counter;
+        char * cs = (char *)s.c_str();
+        int size = s.length();
+        for (int i = 0; i < size; i++) {
+            if (counter.find(cs[i]) == counter.end()) {
+                counter[cs[i]] = 1;
+            } else {
+                counter[cs[i]] += 1;
+            }
+        }
+        cs = (char *)t.c_str();
+        size = t.length();
+        for (int i = 0; i < size; i++) {
+            if (counter.find(cs[i]) == counter.end()) {
+                counter[cs[i]] = 1;
+            } else {
+                counter[cs[i]] -= 1;
+            }
+        }
+        for (map<char,int>::iterator it = counter.begin(); it != counter.end(); ++it) {
+            if (it->second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
