538b61754fec104d45f460b50ef940e8ced27bbd
Rodolfo P A <rodoufu@gmail.com>
2019-01-30 17:21:20 -0200

    Create ValidParentheses.hpp

---
+++ leetCode/others/ValidParentheses.hpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> mystack;
+        map<char, char> openCloser = {
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'}
+        };
+        int len = s.length();
+        const char* cs = s.c_str();
+        for (int i = 0; i < len; ++i) {
+            if (openCloser.find(cs[i]) != openCloser.end()) {
+                mystack.push(cs[i]);
+            } else {
+                if (mystack.size() == 0 || cs[i] != openCloser[mystack.top()]) {
+                    return false;
+                }
+                mystack.pop();
+            }
+        }
+        return mystack.size() == 0;
+    }
+};
