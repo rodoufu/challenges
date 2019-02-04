8a1e182c1880f2dacbd4abde03dfad8e1dca330c
Rodolfo P A <rodoufu@gmail.com>
2019-02-04 17:20:51 -0200

    Create MergeTwoSortedLists.hpp

---
+++ leetCode/interview/LinkedList/MergeTwoSortedLists.hpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1 && !l2) {
+            return NULL;
+        }
+        ListNode * resp = new ListNode(0);
+        ListNode * it = resp;
+        while (l1 && l2) {
+            it->next = new ListNode(0);
+            if (l1->val < l2->val) {
+                it->val = l1->val;
+                l1 = l1->next;
+            } else {
+                it->val = l2->val;
+                l2 = l2->next;
+            }
+            it = (l1 || l2) ? it->next : it;
+        }
+        while (l1) {
+            it->next = new ListNode(0);
+            it->val = l1->val;
+            l1 = l1->next;
+            it = (l1 || l2) ? it->next : it;
+        }
+        while (l2) {
+            it->next = new ListNode(0);
+            it->val = l2->val;
+            l2 = l2->next;
+            it = (l1 || l2) ? it->next : it;
+        }
+        delete it->next;
+        it->next = NULL;
+        return resp;
+    }
+};
