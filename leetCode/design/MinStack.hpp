af435a8c828e14b5a1201c5eec479c3967241d48
Rodolfo P A <rodoufu@gmail.com>
2019-02-04 10:47:43 -0200

    Create MinStack.hpp

---
+++ leetCode/design/MinStack.hpp
@@ -0,0 +1,41 @@
+class MinStack {
+private:
+    stack<int> data;
+    stack<int> min;
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+    }
+    
+    void push(int x) {
+        data.push(x);
+        if (min.size() == 0) {
+            min.push(x);
+        } else {
+            int v = min.top();
+            min.push(v < x ? v : x);
+        }
+    }
+    
+    void pop() {
+        data.pop();
+        min.pop();
+    }
+    
+    int top() {
+        return data.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
