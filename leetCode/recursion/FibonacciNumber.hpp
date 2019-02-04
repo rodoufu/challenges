b7d7c89093ab13421e068d96efb9aebafbb56098
Rodolfo P A <rodoufu@gmail.com>
2019-02-04 16:56:17 -0200

    Create FibonacciNumber.hpp

---
+++ leetCode/recursion/FibonacciNumber.hpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int N) {
+        if (N == 0) {
+            return 0;
+        }
+        if (N <= 2) {
+            return 1;
+        }
+        int fib[] = {0, 1, 1};
+        for (int i = 2; i < N; ++i) {
+            fib[0] = fib[1];
+            fib[1] = fib[2];
+            fib[2] = fib[0] + fib[1];
+        }
+        return fib[2];
+    }
+};
