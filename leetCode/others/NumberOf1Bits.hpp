9eeed7599974c36de2ed5d737df62c05dd283595
Rodolfo P A <rodoufu@gmail.com>
2019-02-04 10:10:21 -0200

    Create NumberOf1Bits.hpp

---
+++ leetCode/others/NumberOf1Bits.hpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int bitsOne = 0;
+        for (int i = 0; i < 32; i++) {
+            bitsOne += n & 1;
+            n >>= 1;
+        }
+        return bitsOne;
+    }
+};
