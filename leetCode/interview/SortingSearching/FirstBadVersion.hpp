bc4207f4d8732be257b17f16f9702340b8c9e0e2
Rodolfo P A <rodoufu@gmail.com>
2019-02-06 17:45:43 -0200

    Create FirstBadVersion.hpp

---
+++ leetCode/interview/SortingSearching/FirstBadVersion.hpp
@@ -0,0 +1,32 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+inline bool isBadVersionCache(map<long long, bool> &cache, long long version) {
+    auto value = cache.find(version);
+    if (value != cache.end()) {
+        return value->second;
+    }
+    return cache[version] = isBadVersion(version);
+}
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        map<long long, bool> cache;
+        long long inf = 0;
+        long long sup = n;
+        long long meio;
+        while (inf <= sup) {
+            meio = (inf + sup) / 2;
+            if (isBadVersionCache(cache, meio) && !isBadVersionCache(cache, meio - 1)) {
+                return (int) meio;
+            }
+            if (isBadVersionCache(cache, meio)) {
+                sup = meio - 1;
+            } else {
+                inf = meio + 1;
+            }
+        }
+        return n;
+    }
+};
