10486ccc37eb935e4e0b97a69db52987e1ceaa87
Rodolfo P A <rodoufu@gmail.com>
2019-02-06 16:54:35 -0200

    Create MergeSortedArray.hpp

---
+++ leetCode/interview/SortingSearching/MergeSortedArray.hpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int i = m; i < m + n; ++i) {
+            nums1[i] = INT_MAX;
+        }
+        auto it1 = nums1.begin(), it2 = nums2.begin();
+        while (it1 != nums1.end() && it2 != nums2.end()) {
+            if (*it1 <= *it2) {
+                ++it1;
+            } else {
+                it1 = nums1.insert(it1, *it2);
+                ++it2;
+            }
+        }
+        nums1.resize(n + m);
+    }
+};
