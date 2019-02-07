0b5cdec64a5861dbb542df927b02390460f47d5c
Rodolfo P A <rodoufu@gmail.com>
2019-02-07 14:09:29 -0200

    Create SortColors.hpp

---
+++ leetCode/problems/SortColors.hpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        unsigned red = count(nums.begin(), nums.end(), 0);
+        unsigned white = count(nums.begin(), nums.end(), 1);
+        unsigned blue = count(nums.begin(), nums.end(), 2);
+        unsigned i = 0;
+        for (; i < red; ++i) {
+            nums[i] = 0;
+        }
+        for (; i < red + white; ++i) {
+            nums[i] = 1;
+        }
+        for (; i < red + white + blue; ++i) {
+            nums[i] = 2;
+        }
+    }
+};
