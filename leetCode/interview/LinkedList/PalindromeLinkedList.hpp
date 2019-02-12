d2ccac5f38ffd3d68875da686c54cfc528b97ffb
Rodolfo P A <rodoufu@gmail.com>
2019-02-12 14:33:31 -0200

    Create PalindromeLinkedList.hpp

---
+++ leetCode/interview/LinkedList/PalindromeLinkedList.hpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> values;
+        auto it = head;
+        while (it) {
+            values.push(it->val);
+            it = it->next;
+        }
+        it = head;
+        while (it) {
+            if (it->val != values.top()) {
+                return false;
+            }
+            values.pop();
+            it = it->next;
+        }
+        return true;
+    }
+};
