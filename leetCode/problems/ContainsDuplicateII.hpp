d2e375e37a8b13e97331db98851150afa17f4ad2
Rodolfo P A <rodoufu@gmail.com>
2019-02-14 11:27:21 -0200

    Create ContainsDuplicateII.hpp

---
+++ leetCode/problems/ContainsDuplicateII.hpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map<int, vector<int>> valueIndex;
+        int size = nums.size();
+        for (int i = 0; i < size; ++i) {
+            if (valueIndex.find(nums[i]) != valueIndex.end()) {
+                valueIndex[nums[i]].push_back(i);
+            } else {
+                valueIndex.emplace(nums[i], vector<int>({i}));
+            }
+        }
+        
+        for (auto it = valueIndex.begin(); it != valueIndex.end(); ++it) {
+            if (it->second.size() > 1) {
+                auto indexes = it->second;
+                int size = indexes.size();
+                for (int i = 0; i < size; ++i) {
+                    for (int j = i + 1; j < size; ++j) {
+                        if (abs(indexes[i] - indexes[j]) <= k) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
