d074e9512ed5cab8b4b3ce8ddc3ede49bf3835d1
Rodolfo P A <rodoufu@gmail.com>
2019-02-14 11:47:15 -0200

    Create ValidPerfectSquare.hpp

---
+++ leetCode/problems/ValidPerfectSquare.hpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        unsigned long long temp;
+        for (unsigned long long i = 0; i <= num / 2; ++i) {
+            temp = i * i;
+            if (temp == num) {
+                return true;
+            }
+            if (temp > num) {
+                break;
+            }
+        }
+        return false;
+    }
+};
