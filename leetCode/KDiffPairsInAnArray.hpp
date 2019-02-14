a86cf6c08a2aa3c80f4735ae8e3f0133aa34e060
Rodolfo P A <rodoufu@gmail.com>
2019-02-14 13:32:12 -0200

    Create KDiffPairsInAnArray.hpp

---
+++ leetCode/KDiffPairsInAnArray.hpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int count = 0;
+        sort(nums.begin(), nums.end());
+        set<tuple<int, int>> pairs;
+        int size = nums.size();
+        for (int i = 0; i < size - 1; ++i) {
+            for (int j = i + 1; j < size; ++j) {
+                if (abs(nums[i] - nums[j]) == k) {
+                    pairs.insert(make_tuple(nums[i], nums[j]));
+                }
+            }
+        }
+        return pairs.size();
+    }
+};
