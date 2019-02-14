fe80f4af580e735c4c79d98305056c5fb51e46a0
Rodolfo P A <rodoufu@gmail.com>
2019-02-14 15:23:14 -0200

    Create BinarySearch.hpp

---
+++ leetCode/problems/BinarySearch.hpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int from = 0, to = nums.size() - 1;
+        int it = (to + from) / 2;
+        while (from <= to) {
+            if (target == nums[it]) {
+                return it;
+            } else if (target < nums[it]) {
+                to = it - 1;
+            } else {
+                from = it + 1;
+            }
+            it = (to + from) / 2;
+        }
+        return -1;
+    }
+};
