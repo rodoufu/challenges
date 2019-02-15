dec9c7ab85e25d383c46961f52d32559b41ef678
Rodolfo P A <rodoufu@gmail.com>
2019-02-15 16:13:42 -0200

    Create TransposeMatrix.hpp

---
+++ leetCode/problems/TransposeMatrix.hpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        int sizeI = A.size();
+        if (sizeI == 0) {
+            return A;
+        }
+        int sizeJ = A[0].size();
+
+        vector<vector<int>> resp;
+        for (int j = 0; j < sizeJ; ++j) {
+            resp.push_back(vector<int>());
+            for (int i = 0; i < sizeI; ++i) {
+                resp[j].push_back(A[i][j]);
+                //printf("%d ", A[i][j]);
+            }
+            //printf("\n");
+        }
+        
+        return resp;
+    }
+};
