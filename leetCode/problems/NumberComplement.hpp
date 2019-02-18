6f92603f87111509fb93d1dd3813386ba19a2c15
Rodolfo P A <rodoufu@gmail.com>
2019-02-18 11:15:52 -0300

    Create NumberComplement.hpp

---
+++ leetCode/problems/NumberComplement.hpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        long long numLong = num;
+        int bits = 0;
+        while (numLong) {
+            ++bits;
+            numLong >>= 1;
+        }
+        numLong = ~num;
+        return numLong & ((1L << bits) - 1);
+    }
+};
