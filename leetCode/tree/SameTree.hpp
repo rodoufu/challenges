a177dad5ce01c450c7da42d68fbc23dd3c8cc9e9
Rodolfo P A <rodoufu@gmail.com>
2019-02-19 14:20:00 -0300

    Create SameTree.hpp

---
+++ leetCode/tree/SameTree.hpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) {
+            return true;
+        }
+        if (!p || !q || p->val != q->val) {
+            return false;
+        }
+        bool equal = isSameTree(p->left, q->left);
+        equal = equal && isSameTree(p->right, q->right);
+        return equal;
+    }
+};
