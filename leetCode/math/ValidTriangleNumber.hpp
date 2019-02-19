be861fbd7918b88eb544f5ec7e5b275e1695f088
Rodolfo P A <rodoufu@gmail.com>
2019-02-19 13:59:48 -0300

    Create ValidTriangleNumber.hpp

---
+++ leetCode/math/ValidTriangleNumber.hpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int cont = 0;
+        int size = nums.size();
+        
+        for (auto it = nums.begin(); it != nums.end(); ++it) {
+            for (auto jt = it + 1; jt != nums.end(); ++jt) {
+                for (auto kt = jt + 1; kt != nums.end(); ++kt) {
+                    cont += *it + *jt > *kt
+                        && *jt + *kt > *it 
+                        && *it + *kt > *jt
+                        ? 1 : 0;
+                }
+            }
+        }
+        
+        return cont;
+    }
+};
