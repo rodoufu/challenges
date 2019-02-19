e3aa065e7ac5034d92b0a277e61ef2ed14afa81a
Rodolfo P A <rodoufu@gmail.com>
2019-02-19 14:40:40 -0300

    Create HappyNumber.hpp

---
+++ leetCode/math/HappyNumber.hpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    inline int digitSum(int n) {
+        int value = (n % 10) * (n % 10);
+        while (n) {
+            n /= 10;
+            value += (n % 10) * (n % 10);
+        }
+        return value;
+    }
+
+    bool isHappy(int n) {
+        set<int> nums;
+        while (n != 1 && nums.find(n) == nums.end()) {
+            nums.emplace(n);
+            n = digitSum(n);
+        }
+        return n == 1;
+    }
+};
