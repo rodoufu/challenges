806221fff14e112d74366fa37ab3d54aaeb4e83e
Rodolfo P A <rodoufu@gmail.com>
2019-03-20 18:12:12 -0300

    Update NumberOfDiscIntersections.java

--- codility/src/main/java/codility/com/lessons/l06sorting/NumberOfDiscIntersections.java
+++ codility/src/main/java/codility/com/lessons/l06sorting/NumberOfDiscIntersections.java
@@ -1,5 +1,6 @@
 package codility.com.lessons.l06sorting;
 
+//https://app.codility.com/programmers/lessons/6-sorting/number_of_disc_intersections/
 public class NumberOfDiscIntersections {
 	public int solution(int[] A) {
 		return -1;
