f1053afa8d43645a8acf441b9485f7a3754ebc63
Rodolfo P A <rodoufu@gmail.com>
2019-03-20 18:11:30 -0300

    Update Distinct.java

--- codility/src/main/java/codility/com/lessons/l06sorting/Distinct.java
+++ codility/src/main/java/codility/com/lessons/l06sorting/Distinct.java
@@ -6,6 +6,7 @@ import java.util.Set;
 /**
  * 100%
  * @author 08115341622
+ * https://app.codility.com/programmers/lessons/6-sorting/distinct/
  */
 public class Distinct {
 	public int solution(int[] A) {
