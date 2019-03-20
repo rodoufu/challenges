109ee2eee9e60233dd509c9ca60879bdd1c95f89
Rodolfo P A <rodoufu@gmail.com>
2019-03-20 18:29:11 -0300

    Update TieRopes.java

--- codility/src/main/java/codility/com/lessons/l16greedy/TieRopes.java
+++ codility/src/main/java/codility/com/lessons/l16greedy/TieRopes.java
@@ -2,7 +2,7 @@ package codility.com.lessons.l16greedy;
 
 /**
  * 100%
- * @author 08115341622
+ * https://app.codility.com/programmers/lessons/16-greedy_algorithms/tie_ropes/
  */
 public class TieRopes {
 	public int solution(int K, int[] A) {
