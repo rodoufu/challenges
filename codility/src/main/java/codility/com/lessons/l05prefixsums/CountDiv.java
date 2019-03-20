27a116529b7220be057be98f83a36a429d828880
Rodolfo P A <rodoufu@gmail.com>
2019-03-20 18:09:34 -0300

    Update CountDiv.java

--- codility/src/main/java/codility/com/lessons/l05prefixsums/CountDiv.java
+++ codility/src/main/java/codility/com/lessons/l05prefixsums/CountDiv.java
@@ -1,5 +1,6 @@
 package codility.com.lessons.l05prefixsums;
 
+//https://app.codility.com/programmers/lessons/5-prefix_sums/count_div/
 public class CountDiv {
 	public int solution(int A, int B, int K) {
 		return -1;
