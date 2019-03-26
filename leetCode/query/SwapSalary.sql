62744feb1e229bf38a3ab771874cc119fcc781cd
Rodolfo P A <rodoufu@gmail.com>
2019-03-26 18:08:27 -0300

    Create SwapSalary.sql

---
+++ leetCode/query/SwapSalary.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/swap-salary/
+# Write your MySQL query statement below
+update
+    Salary
+set
+    sex = 
+        case sex
+            when 'm' then 'f'
+            else 'm'
+        end
+;
