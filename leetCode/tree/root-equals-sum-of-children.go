54babfb0df76ac8fa8c0933b1a1f935e8329322c
Rodolfo P A <rodoufu@gmail.com>
2022-04-16 11:09:46 -0300

    Create root-equals-sum-of-children.go

---
+++ leetCode/tree/root-equals-sum-of-children.go
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/root-equals-sum-of-children/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func checkTree(root *TreeNode) bool {
+    return root.Val == root.Left.Val + root.Right.Val
+}
