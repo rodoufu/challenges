0d3e5153f497357d4490acaf0fce5c559e73ac74
Rodolfo P A <rodoufu@gmail.com>
2022-04-24 12:10:16 -0300

    Create find-customer-referee.sql

---
+++ leetCode/sql/find-customer-referee.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/find-customer-referee/
+select
+    name
+from
+    customer
+where
+    referee_id <> 2
+    or referee_id is null
+;
