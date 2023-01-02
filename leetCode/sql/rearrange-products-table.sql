8ba16c451a022a80e95adbb58ea86f101d2b1fa5
Rodolfo P A <rodoufu@gmail.com>
2023-01-02 15:37:01 -0300

    Update rearrange-products-table.sql

--- leetCode/sql/rearrange-products-table.sql
+++ leetCode/sql/rearrange-products-table.sql
@@ -1,1 +1,7 @@
 # https://leetcode.com/problems/rearrange-products-table/
+# Write your MySQL query statement belowstoas
+select product_id, 'store1' as store , store1 as price from products where store1 is not null 
+union all
+Select product_id, 'store2', store2 from Products where store2 is not null 
+union all 
+Select product_id, 'store3', store3 from Products where store3 is not null
